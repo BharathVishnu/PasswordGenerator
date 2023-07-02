ee3b330c05ebc2c8499de1b4743d90e85a264116
Bharath Vishnu B <bharathvb1204@gmail.com>
2023-07-02 19:31:24 +0530

    webapp

---
+++ src/Characters.jsx
@@ -0,0 +1,4 @@
+export const num = '0123456789'
+export const alpha = 'qwertyuiopasdfghjklzxcvbnm'
+export const upAlpha = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+export const spChar = "!'^+%&/()=?_#$½§{[]}|;:>÷`<.*-@é"
